8811809799bbc2faa78c83d9d40375dd4374a3ae
Simon Wictorsson <simon.wictorsson@nibe.se>
2018-11-02 15:39:46 +0100

    2015 day 6
    2015 day 6

--- 2015/6/1/index.js
+++ 2015/6/1/index.js
@@ -15,30 +15,24 @@ for (let i = 0; i < 1000; i++) {
 }
 
 data.forEach(el => {
-    switch (el[0]) {
-        case 'turn on':
-            for(let i = el[1]; i <= el[3] ; i++) {
-                for(let j = el[2]; j <= el[4] ; j++) {
+    for(let i = el[1]; i <= el[3] ; i++) {
+        for(let j = el[2]; j <= el[4] ; j++) {
+            switch (el[0]) {
+                case 'turn on':
                     lights[i][j] = true;
-                }
+                    break;
+                case 'turn off':
+                    lights[i][j] = false;
+                    break;
+                case 'toggle':
+                    lights[i][j] = !lights[i][j];
+                    break;
             }
-            break;
-        case 'turn of':
-            for(let i = el[1]; i <= el[3] ; i++) {
-                for(let j = el[2]; j <= el[4] ; j++) {
-                    lights[i][j] = true;
-                }
-            }
-            break;
-        case 'toggle':
-            for(let i = el[1]; i <=el[3] ; i++) {
-                for(let j = el[2]; j <=el[4] ; j++) {
-                    lights[i][j] = lights[i][j] ? false : true;
-                }
-            }
-            break;
+        }
     }
 });
+
+
 
 let on = 0;
 
