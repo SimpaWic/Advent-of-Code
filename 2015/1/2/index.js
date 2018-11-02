5d1b65881593a7ad884cedbea124fc1cb13685f4
Simon Wictorsson <simon.wictorsson@nibe.se>
2018-11-02 07:35:49 +0100

    2015 day 1
    2015 day 1

--- 2015/1/2/index.js
+++ 2015/1/2/index.js
@@ -2,15 +2,14 @@ let fs = require('fs');
 
 let data = fs.readFileSync('../input.txt', 'utf8').split('');
 
-let floor = 0;
-
-let index = 0;
+let floor = 0,
+    index = 0;
 
 data.some( el => {
     el === '(' ? floor++ : floor--;
 
     index++;
-    
+
     return floor === -1;
 });
 
